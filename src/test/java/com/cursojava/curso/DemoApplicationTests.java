ba17369e853ff7483e0ac3c63275df1d690d9065
DavidMonterroso <david.monterroso@eduacademy.ac>
2024-06-01 16:37:36 +0200

    Repositorio de progra3

---
+++ src/test/java/com/cursojava/curso/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
